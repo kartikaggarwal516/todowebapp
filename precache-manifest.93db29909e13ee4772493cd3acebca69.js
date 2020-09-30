9ee4b5d31cc3086c67cf25235f500d28d40720c8
Kartik <aggarwalkartik96@gmail.com>
2020-09-30 20:35:37 +0530

    Updates

---
+++ precache-manifest.93db29909e13ee4772493cd3acebca69.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e56f46f9166a774207ae8a3d5d3e44",
+    "url": "/todowebapp/index.html"
+  },
+  {
+    "revision": "92db4a8b6c55982c1fcc",
+    "url": "/todowebapp/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "66c84706a73928206b29",
+    "url": "/todowebapp/static/css/main.c86b00c5.chunk.css"
+  },
+  {
+    "revision": "92db4a8b6c55982c1fcc",
+    "url": "/todowebapp/static/js/2.d727efb2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/todowebapp/static/js/2.d727efb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66c84706a73928206b29",
+    "url": "/todowebapp/static/js/main.b1b6eda6.chunk.js"
+  },
+  {
+    "revision": "efa5cd27937d3ea2ceab",
+    "url": "/todowebapp/static/js/runtime-main.3a419659.js"
+  },
+  {
+    "revision": "6ff8ef533cc97014429924d3327d98bd",
+    "url": "/todowebapp/static/media/checked.6ff8ef53.svg"
+  },
+  {
+    "revision": "0b454429446d6ff4ab9f3cafa48d7a34",
+    "url": "/todowebapp/static/media/not-checked.0b454429.svg"
+  }
+]);
